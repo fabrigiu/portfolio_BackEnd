db492ec386b48d955da1f04e9017f69020c1b2d9
fabrigiu <fabriciogiudici97@hotmail.com>
2022-10-28 12:56:23 -0300

    primer commit

---
+++ fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.fsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsgApplication.class, args);
+	}
+
+}
---
+++ fsg/src/test/java/com/myportfolio/fsg/FsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.fsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
