ad38b0527b9443b2c20b7db67d45d69f0a38b933
Fabricio Giudici <fabriciogiudici97@hotmail.com>
2022-12-01 15:24:58 -0300

    Update FsgApplication.java

--- fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
+++ fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
@@ -2,8 +2,11 @@ package com.myportfolio.fsg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.myportfolio.fsg.Controller"+"com.myportfolio.fsg.Dto"+"com.myportfolio.fsg.Entity"
++"com.myportfolio.fsg.Interface"+"com.myportfolio.fsg.Repository"+"com.myportfolio.fsg.Security"+"com.myportfolio.fsg.Service"})
 public class FsgApplication {
 
 	public static void main(String[] args) {
