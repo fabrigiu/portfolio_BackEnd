bfce72d46e41a1fd69b60581459f6a0121004005
Fabricio Giudici <fabriciogiudici97@hotmail.com>
2022-12-01 14:02:33 -0300

    didnt work

--- fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
+++ fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
@@ -2,10 +2,10 @@ package com.myportfolio.fsg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+/*import org.springframework.context.annotation.ComponentScan;*/
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.myportfolio.fsg.Controller"})
+/*@ComponentScan(basePackages={"com.myportfolio.fsg.Controller"})*/
 public class FsgApplication {
 
 	public static void main(String[] args) {
