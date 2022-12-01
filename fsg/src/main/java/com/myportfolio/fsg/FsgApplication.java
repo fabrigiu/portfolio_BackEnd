2f0af4bdee539103b8da8dbb602107265a53a96f
Fabricio Giudici <fabriciogiudici97@hotmail.com>
2022-12-01 15:34:03 -0300

    8

--- fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
+++ fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.myportfolio.fsg.Controller"+"com.myportfolio.fsg.Dto"+"com.myportfolio.fsg.Entity"
-+"com.myportfolio.fsg.Interface"+"com.myportfolio.fsg.Repository"+"com.myportfolio.fsg.Security"+"com.myportfolio.fsg.Service"})
++"com.myportfolio.fsg.Interface"+"com.myportfolio.fsg.Repository"+"com.myportfolio.fsg.Security"
+        +"com.myportfolio.fsg.Security.Controller"+"com.myportfolio.fsg.Security.Dto"+"com.myportfolio.fsg.Security.Entity"
+        +"com.myportfolio.fsg.Security.Enums"+"com.myportfolio.fsg.Security.Repository"+"com.myportfolio.fsg.Security.Service"
+        +"com.myportfolio.fsg.Security.jwt"+"com.myportfolio.fsg.Service"})
 public class FsgApplication {
 
 	public static void main(String[] args) {
