2afdfdbb78ddabc976115a8daca5d3fd81850069
Fabricio Giudici <fabriciogiudici97@hotmail.com>
2022-12-01 15:00:59 -0300

    6
    whitelabel fix

--- fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
+++ fsg/src/main/java/com/myportfolio/fsg/FsgApplication.java
@@ -2,9 +2,8 @@ package com.myportfolio.fsg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class FsgApplication {
 
 	public static void main(String[] args) {
